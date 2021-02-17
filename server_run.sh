459f8084bdbf053c8429aa74d2e5d2b696aaa533
SuveerGoyal <69322303+SuveerGoyal@users.noreply.github.com>
2021-02-17 11:57:48 +0530

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -2,7 +2,7 @@ cd backend
 echo 'export PATH=$PATH:/usr/local/bin' >> $HOME/.bashrc
 sudo -u postgres psql -c 'CREATE DATABASE xmeme;'
 sudo -u postgres psql -d 'xmeme' << EOF
-CREATE TABLE public.memes2
+CREATE TABLE memes
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 ),
     name text NOT NULL,
@@ -11,11 +11,7 @@ CREATE TABLE public.memes2
     PRIMARY KEY (id),
     UNIQUE (name, url, caption)
 );
-
-ALTER TABLE public.memes2
-    OWNER to postgres;
 EOF
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'test';"
-
 npm install
 npm start
