fe68a41d826d3387f50244d14bc237fd55fd0b87
SuveerGoyal <69322303+SuveerGoyal@users.noreply.github.com>
2021-02-17 16:31:08 +0530

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,7 +1,7 @@
 cd backend
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'test';"
-sudo -u postgres psql -c 'CREATE DATABASE xmeme;'
-sudo -u postgres psql -d 'xmeme' << EOF
+sudo -u postgres psql -c 'CREATE DATABASE Xmeme;'
+sudo -u postgres psql -d 'Xmeme' << EOF
 CREATE TABLE memes
 (
     id SERIAL PRIMARY KEY,
